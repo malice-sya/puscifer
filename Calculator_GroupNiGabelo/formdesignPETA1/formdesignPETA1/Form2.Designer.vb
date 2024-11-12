ce524c45db021a5c80145e4c37e454a8993786b4
lain <corellon.larethian25@gmail.com>
2024-11-12 18:20:37 +0800

    Add files via upload

---
+++ Calculator_GroupNiGabelo/formdesignPETA1/formdesignPETA1/Form2.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Button1 = New Button()
+        Label12 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Brittany Signature", 47.9999924F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label1.Location = New Point(162, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(298, 132)
+        Label1.TabIndex = 0
+        Label1.Text = "Members:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Ink Free", 25F)
+        Label2.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label2.Location = New Point(181, 396)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(220, 42)
+        Label2.TabIndex = 1
+        Label2.Text = "James Gabelo"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Ink Free", 17F)
+        Label3.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label3.Location = New Point(365, 318)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(184, 29)
+        Label3.TabIndex = 2
+        Label3.Text = "Fritz Louie Sotto"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.BackColor = Color.Transparent
+        Label4.Font = New Font("Ink Free", 17F)
+        Label4.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label4.Location = New Point(61, 189)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(174, 29)
+        Label4.TabIndex = 3
+        Label4.Text = "Jacob Maglipong"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.BackColor = Color.Transparent
+        Label5.Font = New Font("Ink Free", 17F)
+        Label5.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label5.Location = New Point(96, 233)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(97, 29)
+        Label5.TabIndex = 4
+        Label5.Text = "RJ Falco"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.BackColor = Color.Transparent
+        Label6.Font = New Font("Ink Free", 17F)
+        Label6.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label6.Location = New Point(58, 318)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(173, 29)
+        Label6.TabIndex = 5
+        Label6.Text = "Dreyven Briones"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.BackColor = Color.Transparent
+        Label7.Font = New Font("Ink Free", 17F)
+        Label7.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label7.Location = New Point(84, 279)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(127, 29)
+        Label7.TabIndex = 6
+        Label7.Text = "Calvin Sison"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.BackColor = Color.Transparent
+        Label8.Font = New Font("Ink Free", 17F)
+        Label8.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label8.Location = New Point(390, 147)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(141, 29)
+        Label8.TabIndex = 7
+        Label8.Text = "Kobe Legaspi"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.BackColor = Color.Transparent
+        Label9.Font = New Font("Ink Free", 17F)
+        Label9.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label9.Location = New Point(383, 188)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(154, 29)
+        Label9.TabIndex = 8
+        Label9.Text = "Denzel Samiya"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.BackColor = Color.Transparent
+        Label10.Font = New Font("Ink Free", 17F)
+        Label10.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label10.Location = New Point(404, 232)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(106, 29)
+        Label10.TabIndex = 9
+        Label10.Text = "Kim Arias"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.BackColor = Color.Transparent
+        Label11.Font = New Font("Ink Free", 17F)
+        Label11.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label11.Location = New Point(398, 277)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(120, 29)
+        Label11.TabIndex = 10
+        Label11.Text = "Rod Morido"
+        ' 
+        ' Button1
+        ' 
+        Button1.Cursor = Cursors.Hand
+        Button1.Font = New Font("Bahnschrift", 9F)
+        Button1.Location = New Point(12, 649)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 12
+        Button1.Text = "Returrrrn"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.BackColor = Color.Transparent
+        Label12.FlatStyle = FlatStyle.Flat
+        Label12.Font = New Font("Ink Free", 17F)
+        Label12.ForeColor = Color.FromArgb(CByte(230), CByte(211), CByte(181))
+        Label12.Location = New Point(52, 146)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(191, 29)
+        Label12.TabIndex = 11
+        Label12.Text = "Larethian Adriano"
+        ' 
+        ' Form2
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(594, 684)
+        Controls.Add(Button1)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form2"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "f"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label12 As Label
+End Class

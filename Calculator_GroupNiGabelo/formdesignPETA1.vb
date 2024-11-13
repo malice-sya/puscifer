7a91508661446acbbef2ea10673fc0f82741b761
lain <corellon.larethian25@gmail.com>
2024-11-13 10:26:10 +0800

    Add files via upload

---
+++ Calculator_GroupNiGabelo/formdesignPETA1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
---
+++ Calculator_GroupNiGabelo/formdesignPETA1/formdesignPETA1/Form1.Designer.vb
@@ -0,0 +1,432 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Button19 = New Button()
+        Button20 = New Button()
+        PictureBox2 = New PictureBox()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Button21 = New Button()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.BorderStyle = BorderStyle.Fixed3D
+        PictureBox1.Location = New Point(24, 185)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(503, 528)
+        PictureBox1.TabIndex = 1
+        PictureBox1.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.White
+        Button1.FlatStyle = FlatStyle.Popup
+        Button1.Font = New Font("Bahnschrift", 39.75F)
+        Button1.Location = New Point(37, 214)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(104, 82)
+        Button1.TabIndex = 2
+        Button1.Text = "√" & vbCrLf
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.White
+        Button2.FlatStyle = FlatStyle.Popup
+        Button2.Font = New Font("Bahnschrift", 39.75F)
+        Button2.Location = New Point(158, 214)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(106, 82)
+        Button2.TabIndex = 3
+        Button2.Text = "x²"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.AutoSize = True
+        Button3.BackColor = Color.White
+        Button3.FlatStyle = FlatStyle.Popup
+        Button3.Font = New Font("Bahnschrift", 39.75F)
+        Button3.Location = New Point(279, 214)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(106, 82)
+        Button3.TabIndex = 4
+        Button3.Text = "⌫"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.AutoSize = True
+        Button4.BackColor = Color.White
+        Button4.FlatStyle = FlatStyle.Popup
+        Button4.Font = New Font("Bahnschrift", 39.75F)
+        Button4.Location = New Point(417, 214)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(92, 82)
+        Button4.TabIndex = 5
+        Button4.Text = "÷"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.AutoSize = True
+        Button5.BackColor = Color.White
+        Button5.FlatStyle = FlatStyle.Popup
+        Button5.Font = New Font("Bahnschrift", 39.75F)
+        Button5.Location = New Point(417, 316)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(92, 82)
+        Button5.TabIndex = 9
+        Button5.Text = "X"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.White
+        Button6.FlatStyle = FlatStyle.Popup
+        Button6.Font = New Font("Bahnschrift", 39.75F)
+        Button6.Location = New Point(279, 316)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(106, 82)
+        Button6.TabIndex = 8
+        Button6.Text = "9"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.White
+        Button7.FlatStyle = FlatStyle.Popup
+        Button7.Font = New Font("Bahnschrift", 39.75F)
+        Button7.Location = New Point(158, 316)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(106, 82)
+        Button7.TabIndex = 7
+        Button7.Text = "8"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.White
+        Button8.FlatStyle = FlatStyle.Popup
+        Button8.Font = New Font("Bahnschrift", 39.75F)
+        Button8.Location = New Point(37, 316)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(104, 82)
+        Button8.TabIndex = 6
+        Button8.Text = "7"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.White
+        Button9.FlatStyle = FlatStyle.Popup
+        Button9.Font = New Font("Segoe UI", 40F)
+        Button9.Location = New Point(417, 416)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(92, 82)
+        Button9.TabIndex = 13
+        Button9.Text = "-"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.White
+        Button10.FlatStyle = FlatStyle.Popup
+        Button10.Font = New Font("Bahnschrift", 39.75F)
+        Button10.Location = New Point(279, 416)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(106, 82)
+        Button10.TabIndex = 12
+        Button10.Text = "6"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.White
+        Button11.FlatStyle = FlatStyle.Popup
+        Button11.Font = New Font("Bahnschrift", 39.75F)
+        Button11.Location = New Point(158, 416)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(106, 82)
+        Button11.TabIndex = 11
+        Button11.Text = "5"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.White
+        Button12.FlatStyle = FlatStyle.Popup
+        Button12.Font = New Font("Bahnschrift", 39.75F)
+        Button12.Location = New Point(37, 416)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(104, 82)
+        Button12.TabIndex = 10
+        Button12.Text = "4"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.AutoSize = True
+        Button13.BackColor = Color.White
+        Button13.FlatStyle = FlatStyle.Popup
+        Button13.Font = New Font("Bahnschrift", 39.75F)
+        Button13.Location = New Point(417, 516)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(92, 82)
+        Button13.TabIndex = 17
+        Button13.Text = "+"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = Color.White
+        Button14.FlatStyle = FlatStyle.Popup
+        Button14.Font = New Font("Bahnschrift", 39.75F)
+        Button14.Location = New Point(279, 516)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(106, 82)
+        Button14.TabIndex = 16
+        Button14.Text = "3"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = Color.White
+        Button15.FlatStyle = FlatStyle.Popup
+        Button15.Font = New Font("Bahnschrift", 39.75F)
+        Button15.Location = New Point(158, 516)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(106, 82)
+        Button15.TabIndex = 15
+        Button15.Text = "2"
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = Color.White
+        Button16.FlatStyle = FlatStyle.Popup
+        Button16.Font = New Font("Bahnschrift", 39.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button16.Location = New Point(37, 516)
+        Button16.Name = "Button16"
+        Button16.Size = New Size(104, 82)
+        Button16.TabIndex = 14
+        Button16.Text = "1"
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = Color.White
+        Button17.FlatStyle = FlatStyle.Popup
+        Button17.Font = New Font("Bahnschrift", 39.75F)
+        Button17.ForeColor = Color.Black
+        Button17.Location = New Point(417, 614)
+        Button17.Name = "Button17"
+        Button17.Size = New Size(92, 82)
+        Button17.TabIndex = 21
+        Button17.Text = "="
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' Button18
+        ' 
+        Button18.BackColor = Color.White
+        Button18.FlatStyle = FlatStyle.Popup
+        Button18.Font = New Font("Bahnschrift", 39.75F)
+        Button18.Location = New Point(279, 614)
+        Button18.Name = "Button18"
+        Button18.Size = New Size(106, 82)
+        Button18.TabIndex = 20
+        Button18.Text = "."
+        Button18.UseVisualStyleBackColor = False
+        ' 
+        ' Button19
+        ' 
+        Button19.BackColor = Color.White
+        Button19.FlatStyle = FlatStyle.Popup
+        Button19.Font = New Font("Bahnschrift", 39.75F)
+        Button19.Location = New Point(158, 614)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(106, 82)
+        Button19.TabIndex = 19
+        Button19.Text = "0"
+        Button19.UseVisualStyleBackColor = False
+        ' 
+        ' Button20
+        ' 
+        Button20.BackColor = Color.White
+        Button20.FlatStyle = FlatStyle.Popup
+        Button20.Font = New Font("Bahnschrift", 39.75F)
+        Button20.Location = New Point(37, 614)
+        Button20.Name = "Button20"
+        Button20.Size = New Size(104, 82)
+        Button20.TabIndex = 18
+        Button20.Text = "CE"
+        Button20.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.BorderStyle = BorderStyle.Fixed3D
+        PictureBox2.Location = New Point(22, 48)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(504, 88)
+        PictureBox2.TabIndex = 22
+        PictureBox2.TabStop = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Bahnschrift", 39.75F)
+        TextBox1.Location = New Point(37, 57)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(476, 71)
+        TextBox1.TabIndex = 23
+        TextBox1.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Label1
+        ' 
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Bahnschrift", 15F)
+        Label1.ImageAlign = ContentAlignment.MiddleRight
+        Label1.Location = New Point(598, 56)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(40, 29)
+        Label1.TabIndex = 24
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Bahnschrift", 15F)
+        Label2.Location = New Point(645, 98)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(42, 37)
+        Label2.TabIndex = 25
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Button21
+        ' 
+        Button21.BackColor = Color.White
+        Button21.Cursor = Cursors.Hand
+        Button21.FlatStyle = FlatStyle.Popup
+        Button21.Font = New Font("Bahnschrift", 9F)
+        Button21.ForeColor = SystemColors.ControlText
+        Button21.Location = New Point(465, 723)
+        Button21.Name = "Button21"
+        Button21.Size = New Size(75, 23)
+        Button21.TabIndex = 26
+        Button21.Text = "About"
+        Button21.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DarkGray
+        ClientSize = New Size(548, 758)
+        Controls.Add(Button21)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(PictureBox2)
+        Controls.Add(Button17)
+        Controls.Add(Button18)
+        Controls.Add(Button19)
+        Controls.Add(Button20)
+        Controls.Add(Button13)
+        Controls.Add(Button14)
+        Controls.Add(Button15)
+        Controls.Add(Button16)
+        Controls.Add(Button9)
+        Controls.Add(Button10)
+        Controls.Add(Button11)
+        Controls.Add(Button12)
+        Controls.Add(Button5)
+        Controls.Add(Button6)
+        Controls.Add(Button7)
+        Controls.Add(Button8)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Calculator"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button21 As Button
+
+End Class
